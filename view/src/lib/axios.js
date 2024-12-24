cde2aac128f27f8413b22fb68d319c9243930d2c
Pruthvij008 <desaipruthvij1@gmail.com>
2024-12-24 11:29:02 +0530

    Initial commit

--- view/src/lib/axios.js
+++ view/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api/v1",
+  baseURL: "",
   // import.meta.env.MODE === "development"
   //   ? "http://localhost:3000/api/v1":
 
