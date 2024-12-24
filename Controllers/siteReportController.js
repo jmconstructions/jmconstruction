e7598a9da2e4309d7b650a4640bd98e2abda52c6
Pruthvij008 <desaipruthvij1@gmail.com>
2024-12-24 18:24:25 +0530

    Initial commit

--- Controllers/siteReportController.js
+++ Controllers/siteReportController.js
@@ -291,13 +291,11 @@ exports.generatePDF = catchAsync(async (req, res, next) => {
   let browser = null;
   const { id } = req.params;
   // const url = `http://localhost:5173/pdf/${id}`;
-  const url = `https://jmconstruction.onrender.com/pdf/${id}`;
-  //change here url
+  const url = `https://jmconstruction-1.onrender.com/pdf/${id}`;
 
   try {
     browser = await puppeteer.launch({
-      executablePath: "/usr/bin/chromium-browser",
-      headless: "new",
+      headless: true,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
